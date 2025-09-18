3a7027507dd654b647fa8da5f028e6b2f5989f7d
denmigda <denis.migdal@uca.fr>
2025-09-18 17:04:18 +0200

    Fix issue

--- src/customElements/whenUpgraded.ts
+++ src/customElements/whenUpgraded.ts
@@ -1,4 +1,8 @@
 export default async function whenUpgraded<T extends HTMLElement>(e: Element) {
+    
+    if( e.ownerDocument !== document )
+        document.adoptNode(e);
+    
     if( customElements.get(e.localName) === undefined )
         await customElements.whenDefined(e.localName);
     customElements.upgrade(e);
