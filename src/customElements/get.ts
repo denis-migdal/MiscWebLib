149b500adf06b4c6155ce497dc3d03db1f4031d6
denmigda <denis.migdal@uca.fr>
2025-09-18 17:09:14 +0200

    Minor change

--- src/customElements/get.ts
+++ src/customElements/get.ts
@@ -1,10 +1,12 @@
-export default async function getUpgraded<T extends HTMLElement>(e: Element): Promise<T> {
+export default async function get<T extends HTMLElement>(e: Element)
+                                                             : Promise<T> {
     
     if( e.ownerDocument !== document )
         document.adoptNode(e);
     
     if( customElements.get(e.localName) === undefined )
         await customElements.whenDefined(e.localName);
+    
     customElements.upgrade(e);
 
     return e as any;
