84dcff8a4554595ae8346ca83a3642572b4873aa
denmigda <denis.migdal@uca.fr>
2025-09-19 07:47:05 +0200

    Add customElements sync get

--- src/customElements/get.ts
+++ src/customElements/get.ts
@@ -1,13 +1,25 @@
-export default async function get<T extends HTMLElement>(e: Element)
-                                                             : Promise<T> {
+export default function get<T extends HTMLElement>(e    : Element,
+                                                   sync?: false
+                                                ) : Promise<T>
+export default function get<T extends HTMLElement>(e   : Element,
+                                                   sync: true
+                                                ) : T
+export default function get<T extends HTMLElement>(e   : Element,
+                                                   sync: boolean = true
+                                                ) : T|Promise<T> {
     
     if( e.ownerDocument !== document )
         document.adoptNode(e);
-    
-    if( customElements.get(e.localName) === undefined )
-        await customElements.whenDefined(e.localName);
-    
-    customElements.upgrade(e);
 
-    return e as any;
+    if( customElements.get(e.localName) !== undefined ) {
+        customElements.upgrade(e);
+        return e as any;
+    }
+
+    if( sync === true)
+        throw new Error(`${e.localName} isn't defined!`);
+
+    const p = Promise.withResolvers<T>();
+    customElements.whenDefined(e.localName).then( () => p.resolve(e as any) );
+    return p.promise;
 }
