aba69661d0302dd0c09ebaeae9e4cde0d675380d
denmigda <denis.migdal@uca.fr>
2026-02-06 10:32:17 +0100

    Move WebComp stuff back to LISS

--- src/customElements/content/addStyles.ts
+++
@@ -1,3 +0,0 @@
-export default function addStyles(shadow: ShadowRoot, styles: CSSStyleSheet[]) {
-    shadow.adoptedStyleSheets.push( ...styles );
-}
--- src/customElements/content/parsers/types.ts
+++
@@ -1,7 +0,0 @@
-export type Template<T> = [TemplateStringsArray, ...string[]]|[T];
-
-export function isTemplateString(
-                                    raw: [unknown, ...unknown[]]
-                                ): raw is [TemplateStringsArray, ...string[]] {
-    return Array.isArray(raw[0]);
-}
